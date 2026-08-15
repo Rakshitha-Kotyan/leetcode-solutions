63688598cefde905cdfdcb09112190cad05b2a61
Rakshitha-Kotyan <rakshithakotyan0@gmail.com>
2026-08-15 17:07:48 +0530

    Time: 15 ms (6.46%), Space: 52.6 MB (6.23%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,22 +1,19 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        Set<String> s=new HashSet<>();
-
+        Set<String> s = new HashSet<>();
         for(int i=0;i<9;i++){
             for(int j=0;j<9;j++){
-                char num=board[i][j];
-                if(num!='.'){
-
-                   if( !s.add(num+" at row "+i) ||
-                    !s.add(num+" at column "+j) ||
-                    !s.add(num+" at box "+i/3+"-"+j/3)){
+                char c=board[i][j];
+                if(c!='.'){
+                    if(!s.add(c+"is at row"+i) ||
+                    !s.add(c+"is at col"+j)||
+                    !s.add(c+"is at box"+i/3+" "+j/3)){
                         return false;
                     }
                 }
+               
             }
-
         }
-
-      return true;
+         return true;
     }
 }

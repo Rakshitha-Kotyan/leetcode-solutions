9ec56973baee19d1a36d0b09094fb1cda4b43a6e
Rakshitha-Kotyan <rakshithakotyan0@gmail.com>
2026-08-17 22:13:14 +0530

    Time: 4 ms (42.18%), Space: 69.7 MB (45.31%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
@@ -6,15 +6,13 @@ class Solution {
             sum+=nums[i];
         }
         maxAvg=(double) sum/k;
-        System.out.println(maxAvg);
-        System.out.println(sum);
-    System.out.println("----------------------------------");
+        
         for(int i=0;i<nums.length-k;i++){
             sum-=nums[i];
             sum+=nums[i+k];
-            System.out.println(sum);
+          
             maxAvg=Math.max(maxAvg,(double) sum/k);
-            System.out.println(maxAvg);
+            
         }
         return maxAvg;
     }

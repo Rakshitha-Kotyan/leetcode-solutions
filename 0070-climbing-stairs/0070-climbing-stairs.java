60eb0fda602c3a7b03b8aac77d8edab62e2f5824
Rakshitha-Kotyan <rakshithakotyan0@gmail.com>
2026-08-20 15:30:13 +0530

    Time: 0 ms (100%), Space: 42.2 MB (36.55%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,17 @@
+class Solution {
+
+    public int climbStairs(int n) {
+        int[] memo = new int[n + 1]; // Include index n
+        return climb(n, memo);
+    }
+
+    private int climb(int n, int[] memo) {
+        if (n <= 2)
+            return n;
+        if (memo[n] != 0)
+            return memo[n];
+        
+        memo[n] = climb(n - 1, memo) + climb(n - 2, memo);
+        return memo[n];
+    }
+}
